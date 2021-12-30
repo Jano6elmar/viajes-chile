ac6066fb10828a12368b3f9a97a024e4fccb9dc2
JanoBelmar1 <alejandro@healthchainhq.com>
2021-12-30 12:16:14 -0300

    Orden del codigo, mas comentarios agregados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
 
 
-
+//Integración de tooltip usado en la sección quienes somos y destacados
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 
-$(document).ready(function(){    
-
+//cambios interactivos en el documento hmtl
+$(document).ready(function(){
+    //cambio de color de los div de los icons de la sección quienes somos
     $(".blue-div").dblclick(function(){
-    $(this).addClass('div-negro');
-        });
-   
+        $(this).addClass('div-negro');
+    });
+    //para ocultar los parrafos de la sección quienes somos
     $("#icon1").click(function(){
         $("#parrafo1").toggle( );         
-        });
+    });
     $("#icon2").click(function(){
         $("#parrafo2").toggle( );         
-        });
+    });
     $("#icon3").click(function(){
         $("#parrafo3").toggle( );         
-        });
-    })
+    });
+})
     
