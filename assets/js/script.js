0bafd3b77f84bd854c3bac4b3cb0699d4c934e58
JanoBelmar1 <alejandro@healthchainhq.com>
2021-12-30 05:55:21 -0300

    algo javascrip agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
 
 
 
-/* $(document).ready(function(){   
-    $("a").on('click', function(event) {
-        if (this.hash !== "") { 
-           event.preventDefault();
-           let hash = this.hash;
-           $('html, body').animate({
-                scrollTop: $(hash).offset().top
-           }, 800, function(){
-           window.location.hash = hash;
-           });
-        } // End if
-    });
-}); */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+$(document).ready(function(){    
+
+    $(".blue-div").dblclick(function(){
+    $(this).addClass('div-negro');
+        });
+   
+    $("#icon1").click(function(){
+        $("#parrafo1").toggle( );         
+        });
+    $("#icon2").click(function(){
+        $("#parrafo2").toggle( );         
+        });
+    $("#icon3").click(function(){
+        $("#parrafo3").toggle( );         
+        });
+    })
+    
